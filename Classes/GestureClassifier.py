5aca6f5d1034a116597f5705fa129bfff4266fb0
Yauhen Yavorski <yauheny@uia.no>
2024-02-07 12:37:12 +0100

    ♻️ Refactor GestureClassifier imports and super call

--- Classes/GestureClassifier.py
+++ Classes/GestureClassifier.py
@@ -4,17 +4,16 @@ The class initializes the NN and performs forward propagation.
 The class was created to be imported by other modules.
 """
 
-import torch.nn as nn
+from torch import nn
 
 
 class GestureClassifier(nn.Module):
-
     """
     The class contains the NN used to classify gestures based on provided data.
     """
 
     def __init__(self, input_layer_size, hidden_layer_size, output_layer_size):
-        super(GestureClassifier, self).__init__()
+        super().__init__()
         self.fc1 = nn.Linear(input_layer_size, hidden_layer_size)
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(hidden_layer_size, output_layer_size)
