8405557455b13fd3ef8d07b41f578df3e3585ecc
Joel <114958636+joelmato@users.noreply.github.com>
2024-02-21 11:40:02 +0100

    🚨 Add pygame initialization and key capture functionality

--- drone/KeyboardTelloModule.py
+++ drone/KeyboardTelloModule.py
@@ -1,19 +1,23 @@
+"""This module is used to capture keyboard inputs using PyGame."""
+
 import pygame
 
 
 def init():
+    """Initialize Pygame Library and Set Control Display as 400x400 pixel."""
     # initialize pygame library
     pygame.init()
     # Set Control Display as 400x400 pixel
-    windows = pygame.display.set_mode((400, 400))
+    pygame.display.set_mode((400, 400))
 
 
 def getKey(KeyName):
+    """Return True if the Key is Pressed, False Otherwise."""
     ans = False
-    for ene in pygame.event.get():
+    for _ in pygame.event.get():
         pass
     KeyInput = pygame.key.get_pressed()
-    mykey = getattr(pygame, "K_{}".format(KeyName))
+    mykey = getattr(pygame, "K_" + KeyName)
 
     if KeyInput[mykey]:
         ans = True
@@ -23,6 +27,7 @@ def getKey(KeyName):
 
 
 def main():
+    """Print the Key Pressed."""
     while getKey("LEFT"):
         print("Left key pressed")
 
